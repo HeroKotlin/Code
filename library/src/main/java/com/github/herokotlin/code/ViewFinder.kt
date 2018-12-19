e03715a4a7d6c19a14e072be39f21447261bb57f
musicode <musicode@qq.com>
2018-12-19 23:57:58 +0800

    update

--- library/src/main/java/com/github/herokotlin/code/ViewFinder.kt
+++ library/src/main/java/com/github/herokotlin/code/ViewFinder.kt
@@ -6,7 +6,7 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
 
-class ViewFinder : View {
+internal class ViewFinder : View {
 
     companion object {
 
