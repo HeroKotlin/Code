07e0e5ed463593783cc6822e1545b090ea3bac77
musicode <musicode@qq.com>
2020-04-07 12:12:03 +0800

    bump 0.2.0

--- library/src/main/java/com/github/herokotlin/code/CodeScanner.kt
+++ library/src/main/java/com/github/herokotlin/code/CodeScanner.kt
@@ -1,10 +1,8 @@
 package com.github.herokotlin.code
 
-import android.Manifest
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
-import android.app.Activity
 import android.content.Context
 import android.graphics.RectF
 import android.util.AttributeSet
@@ -12,7 +10,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.LinearInterpolator
 import android.widget.RelativeLayout
-import com.github.herokotlin.permission.Permission
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.ResultPoint
 import com.journeyapps.barcodescanner.BarcodeCallback
@@ -23,9 +20,6 @@ import kotlinx.android.synthetic.main.code_scanner.view.*
 import java.lang.Exception
 
 open class CodeScanner: RelativeLayout {
-
-    // 用于请求权限
-    var activity: Activity? = null
 
     var guideTitle = ""
 
@@ -39,11 +33,7 @@ open class CodeScanner: RelativeLayout {
 
     lateinit var callback: CodeScannerCallback
 
-    private val permission = Permission(19904, listOf(Manifest.permission.CAMERA))
-
     private var supportedCodeType = listOf(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_128)
-
-    private var hasPermission = false
 
     private var isTorchOn = false
 
@@ -149,7 +139,6 @@ open class CodeScanner: RelativeLayout {
             override fun cameraError(error: Exception?) {
                 isTorchOn = false
                 isPreviewing = false
-                callback.onPermissionsNotGranted()
             }
 
             override fun previewStopped() {
@@ -187,20 +176,6 @@ open class CodeScanner: RelativeLayout {
             }
         })
 
-        permission.onRequestPermissions = { activity, permissions, requestCode ->
-            callback.onRequestPermissions(activity, permissions, requestCode)
-        }
-        permission.onPermissionsNotGranted = {
-            callback.onPermissionsNotGranted()
-        }
-        permission.onPermissionsGranted = {
-            hasPermission = true
-            callback.onPermissionsGranted()
-        }
-        permission.onPermissionsDenied = {
-            callback.onPermissionsDenied()
-        }
-
     }
 
     private fun startLaser() {
@@ -229,16 +204,9 @@ open class CodeScanner: RelativeLayout {
         laserAnimator = null
     }
 
-    private fun requestPermissions() {
-        val context = activity ?: (context as Activity)
-        permission.requestPermissions(context) {
-            barcodeView.resume()
-        }
-    }
-
     fun start() {
         barcodeView.decodeContinuous(barcodeCallback)
-        requestPermissions()
+        barcodeView.resume()
     }
 
     fun stop() {
@@ -253,12 +221,7 @@ open class CodeScanner: RelativeLayout {
      * Call from UI thread only.
      */
     fun resume() {
-        // 未知、没权限、拒绝权限三种情况下，resume 都不用再次发起请求
-        // 因为安卓权限请求弹窗消失后，会立即触发 resume，而后才知道是哪种权限状态（即存在顺序问题）
-        // 这就会导致再次调用 requestPermissions() 时，和第一次调用 requestPermissions() 时相同的状态
-        if (hasPermission) {
-            requestPermissions()
-        }
+        barcodeView.resume()
     }
 
     /**
@@ -268,10 +231,6 @@ open class CodeScanner: RelativeLayout {
      */
     fun pause() {
         barcodeView.pause()
-    }
-
-    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        permission.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
 }
--- library/src/main/java/com/github/herokotlin/code/CodeScannerCallback.kt
+++ library/src/main/java/com/github/herokotlin/code/CodeScannerCallback.kt
@@ -1,28 +1,9 @@
 package com.github.herokotlin.code
 
-import android.app.Activity
-import androidx.core.app.ActivityCompat
-
 interface CodeScannerCallback {
 
     // 识别成功
     fun onScanSuccess(text: String) {
-
-    }
-
-    fun onRequestPermissions(activity: Activity, permissions: Array<out String>, requestCode: Int) {
-        ActivityCompat.requestPermissions(activity, permissions, requestCode)
-    }
-
-    fun onPermissionsGranted() {
-
-    }
-
-    fun onPermissionsDenied() {
-
-    }
-
-    fun onPermissionsNotGranted() {
 
     }
 
